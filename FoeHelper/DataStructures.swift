b7a66e5ad12031c20d96e23c228e00a7c4e293d4
George Karavas <support@gkaravas.com>
2021-10-10 17:21:42 +0300

    Version: 1.2(1)

--- FoeHelper/DataStructures.swift
+++ FoeHelper/DataStructures.swift
@@ -7,13 +7,15 @@
 
 import Foundation
 
-let urlString = "http://127.0.0.1:8080/gkaravas/foeHelper/external/postman"
+//let urlString = "http://127.0.0.1:8080/gkaravas/foeHelper/external/postman"
 //let urlString = "http://192.168.1.22:8080/gkaravas/foeHelper/external/postman"
-//let urlString = "https://www.gkaravas.com/foeHelper/external/postman"
+let urlString = "https://www.gkaravas.com/foeHelper/external/postman"
+
+
 public struct Technology: Codable, Identifiable{
     public var code: String
     public var id: Int
-    public var amount: String?
+    public var amount: Int?
     public var points: Int?
     public var goods: [Good]?
 }
@@ -24,7 +26,7 @@ public struct Good: Codable, Identifiable{
     public var age: String?
     public var technologies: [Technology]?
     public var totalTechAmount: Int?
-    public var amount: String?
+    public var amount: Int?
 }
 
 public struct Age: Codable, Identifiable{
